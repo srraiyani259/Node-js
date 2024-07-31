2738a47ccb6b39075ec15726fc0c14452bb4e1fe
srraiyani259 <srraiyani259@gmail.com>
2024-07-31 14:47:31 +0530

    30-7-24

---
+++ Http/index.js
@@ -0,0 +1,23 @@
+
+const http = require("http");
+const fsPromise = require("fs/promises");
+const server = http.createServer(async (req, res) => {
+  const path = req.url;
+  const method = req.method;
+
+
+  console.log(new Date(), path, method);
+
+
+  if (path.includes("file") && method == "GET") {
+    const filename = path.split("/").pop(); // index.html
+    console.log(filename);
+    const data = await fsPromise.readFile("./" + filename);
+    res.end(data);
+  }
+});
+
+
+server.listen(6700, () => {
+  console.log("server running");
+});
