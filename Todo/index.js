c4a9bba64e11a7489c12747e3c59c9ded513ada3
srraiyani259 <srraiyani259@gmail.com>
2024-08-07 14:40:12 +0530

    6-8-24

---
+++ Todo/index.js
@@ -0,0 +1,100 @@
+const express=require('express');
+const port=7000;
+const app=express();
+
+app.use(express.urlencoded());
+app.use(express.static("public"));
+
+app.set("view engine","ejs");
+
+let studentdata=[
+  {
+    id:1,
+    name:"Shreyas"
+  },
+  {
+    id:2,
+    name:"Jenil"
+  }
+]
+
+const middleware=async(req,resp,next)=>{
+
+   if(req.query.age >= 18){
+        next();
+
+   } else {
+     resp.redirect("/")
+   }
+}
+
+
+
+app.get("/",(req,resp)=>{
+    resp.render("index",{studentdata});
+});
+
+app.post('/showdata',(req,resp)=>{
+  const {id,name}=req.body;
+  let student_obj={
+    id:id,
+    name:name
+  }
+  studentdata.push(student_obj);
+  resp.redirect("/")  
+})
+
+app.get("/deletedata",(req,resp)=>{
+    const id = req.query.userid;
+
+    const data=studentdata.filter((el,i)=>{
+     return el.id !=id;
+    })
+    studentdata=data;
+    resp.redirect("back")
+})
+
+app.get("/editdata",(req,resp)=>{
+   
+  const id=req.query.editid;
+
+  const data=studentdata.find(el=>el.id==id)
+  
+   
+   resp.render("edit",{
+    data
+   })
+  
+   
+});
+
+
+
+
+app.post("/updatedata",(req,resp)=>{
+   const {id,name}=req.body;
+
+   const student_id=parseInt(id);
+
+   const student=studentdata.find(el=>el.id===student_id);
+
+   if(student){
+    student.name=name;
+   }
+   resp.redirect("/");
+});
+
+app.get("/home",middleware,(req,resp)=>{
+  resp.render("home");
+});
+
+app.get("/admin",(req,resp)=>{
+  resp.render("admin")
+});
+
+app.get("/registar",(req,resp)=>{
+  resp.render("registar")
+});
+
+app.use(middleware);
+app.listen(port, console.log(`Server Started by port ${port} .....`));
