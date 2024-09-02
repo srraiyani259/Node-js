bc8a8f15a0dffb360699adeb513b763e7d52fa70
srraiyani259 <srraiyani259@gmail.com>
2024-09-02 23:54:07 +0530

    MVC Structure(2-9-24)

---
+++ CRUD-mvc/controllers/adminCtl.js
@@ -0,0 +1,11 @@
+module.exports.form = (req , res)=>{
+    res.render("form")
+}
+
+module.exports.home = (req , res)=>{
+    res.render("home")
+}
+
+module.exports.about = (req , res)=>{
+    res.render("about")
+}
